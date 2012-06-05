1caf5dca339e44aa8665ebac5cfbc88bbbeabc7a
gogoprog <gogoprog@gmail.com>
2012-06-05 17:33:59 +0200

    Added premake file.

---
+++ Build/premake4.lua
@@ -0,0 +1,28 @@
+solution "libRocketCircularBar"
+    configurations { "Debug", "Release" } 
+
+    project "libRocketCircularBar"
+    
+        rocket_dir = os.getenv( "LIBROCKET" ) .. "/"
+        
+        defines { "STATIC_LIB" }
+        kind "StaticLib"
+        language "C++"
+        files { "../Source/CircularBar/**.cpp", "../Source/CircularBar/**.h" }
+        includedirs{ rocket_dir .. "Include/", "../Include/" }
+        
+        
+        configuration "Debug"
+            targetname "RocketCircularBar_d"
+            libdirs{ rocket_dir .. "Build/Debug" }
+            defines { "DEBUG" }
+            flags { "Symbols" }
+            links { "RocketCore_d" }
+            
+        configuration "Release"
+            targetname "RocketCircularBar"
+            libdirs{ rocket_dir .. "Build/Release" }
+            defines { "NDEBUG" }
+            links { "RocketCore" }
+            
+        configuration {}
